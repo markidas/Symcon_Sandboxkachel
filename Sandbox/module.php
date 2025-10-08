54523b1922db9c4c1abe331ba426802b60bc6937
markidas <121538192+markidas@users.noreply.github.com>
2025-10-08 18:52:52 +0200

    Update module.php

--- Sandbox/module.php
+++ Sandbox/module.php
@@ -7,6 +7,11 @@ declare(strict_types=1);
 		{
 			//Never delete this line!
 			parent::Create();
+
+			$this->RegisterVariableBoolean('Switch','Switch');
+			$this->EnableAction('Switch');
+
+			$this->SetVisualizationType(1);
 		}
 
 		public function Destroy()
@@ -20,4 +25,17 @@ declare(strict_types=1);
 			//Never delete this line!
 			parent::ApplyChanges();
 		}
+
+		public function RequestAction($Ident, $Value) {
+			switch($Ident) {
+				case 'Switch':
+					$this->SetValue($Ident, $Value);
+					break;
+			}
+		}	
+	}
+
+	public function GetVisualizationTile() {
+		$htmlFile = file_get_contents(__DIR__ . '/module.html')
+		return $htmlFile;
 	}
